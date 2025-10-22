de99ab90f39f6593daac8564b180d86c4bd3c990
jwalsch <jonwal06de@gmail.com>
2025-10-22 15:52:19 +0200

    version1.1

--- mensa-1.0/mensa.py
+++ mensa-1.0/mensa.py
@@ -1,10 +1,13 @@
-
 #!/usr/bin/env python3
-
+import argparse
 from bs4 import BeautifulSoup
 import requests
 from datetime import date
 
+parser = argparse.ArgumentParser(description="parser")
+parser.add_argument("--today", action="store_true", help="Speiseplan heute")
+parser.add_argument("--week", action="store_true", help="Speiseplan woche")
+args = parser.parse_args()
 # URL der Mensa
 url = "https://www.mensaplan.de/potsdam/mensa-griebnitzsee/index.html"
 
@@ -18,10 +21,13 @@ heute = date.today().strftime("%d.%m.%Y")
 # Tabelle mit Speiseplan finden
 tabelle = soup.find("table", class_="aw-weekly-menu")
 
-
-today_cells = tabelle.find_all("td",class_="today")
-print(today_cells[1].text)
-print(today_cells[2].text)
-print(today_cells[5].text)
-print(today_cells[6].text)
+if args.today:
+    today_cells = tabelle.find_all("td",class_="today")
+    print(today_cells[6].text)
+    print(today_cells[8].text)
+    print(today_cells[12].text)
+    print(today_cells[13].text)
+elif args.week:
+    for td in tabelle.find_all("td"):
+        print(td.text)
 
