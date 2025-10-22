3007e34d582b7d50f78ee6906d497062df15b227
jwalsch <jonwal06de@gmail.com>
2025-10-22 16:32:29 +0200

    version 2

--- mensa-1.0/mensa.py
+++ mensa-1.0/mensa.py
@@ -7,6 +7,7 @@ from datetime import date
 parser = argparse.ArgumentParser(description="parser")
 parser.add_argument("--today_gr", action="store_true", help="Speiseplan heute griebnitzsee")
 parser.add_argument("--today_go", action="store_true", help="Speiseplan heute golm")
+parser.add_argument("--today_np", action="store_true", help="Speiseplan heute neues palais")
 parser.add_argument("--week", action="store_true", help="Speiseplan woche")
 args = parser.parse_args()
 # URL der Mensa
@@ -14,6 +15,8 @@ if args.today_gr:
     uni = "griebnitzsee"
 elif args.today_go:
     uni = "golm"
+elif args.today_np:
+    uni = "am-neuen-palais"
 else:
     raise SystemExit("bitte --today-gr oder today-go eingeben")
 url = f"https://www.mensaplan.de/potsdam/mensa-{uni}/index.html"
@@ -35,6 +38,12 @@ if args.today_gr:
     print(today_cells[12].text)
     print(today_cells[13].text)
 elif args.today_go:
+    today_cells = tabelle.find_all("td",class_="today")
+    print(today_cells[1].text)
+    print(today_cells[2].text)
+    print(today_cells[3].text)
+    print(today_cells[4].text)
+elif args.today_np:
     today_cells = tabelle.find_all("td",class_="today")
     print(today_cells[1].text)
     print(today_cells[2].text)
